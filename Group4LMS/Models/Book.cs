37fca8e8f4dba5bfb18e42b8b1c3945494c4a5a3
vicfer00 <vicfer00@gmail.com>
2019-09-16 19:35:56 -0500

    Added Search by Title and Genre

--- Group4LMS/Models/Book.cs
+++ Group4LMS/Models/Book.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Group4LMS.Models
 {
@@ -14,9 +15,12 @@ namespace Group4LMS.Models
         public string ISBN10 { get; set; }
         public string ISBN13 { get; set; }
         public string Genre { get; set; }
+        [Display(Name = "Available")]
         public char isAvailable { get; set; }
+        [Display(Name = "Reserved Date")]
         [DataType(DataType.Date)]
         public DateTime reservedDate { get; set; }
+        [Display(Name = "Return Date")]
         [DataType(DataType.Date)]
         public DateTime returnDate { get; set; }
 
--- Group4LMS/Pages/Books/Index.cshtml.cs
+++ Group4LMS/Pages/Books/Index.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Group4LMS.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Group4LMS.Pages.Books
 {
@@ -20,9 +21,33 @@ namespace Group4LMS.Pages.Books
 
         public IList<Book> Book { get;set; }
 
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string BookGenre { get; set; }
+
         public async Task OnGetAsync()
         {
-            Book = await _context.Book.ToListAsync();
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Book
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var books = from b in _context.Book
+                         select b;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                books = books.Where(s => s.Title.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(BookGenre))
+            {
+                books = books.Where(x => x.Genre == BookGenre);
+            }
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Book = await books.ToListAsync();
         }
     }
 }
